fc7788a2a03cb0c14fcb15cdb4a883de55dcff3e
STALIN ESPINOZA CALLE <sespinozac@unemi.edu.ec>
2024-01-22 18:52:13 -0500

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,6 +35,7 @@ import { ResetComponent } from './pages/reset/reset.component';
     ResetComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
